a857d17d086fd63bf385cb9e6f0aa9edc792b1fa
Ray Aurelle P Puyat <rpuyat@umich.edu>
2020-04-06 17:31:19 -0400

    Initial implementation of ft_fft

--- src/features/ft_fft.py
+++ src/features/ft_fft.py
@@ -10,9 +10,9 @@ def featurize(data):
     ret = []
     grayData = []
     fftData = []
-    
+
     grayData = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in data]
     fftData = [np.fft.fft(img) for img in grayData]
     ret = [img.flatten() for img in fftData]
-    
+
     return ret
