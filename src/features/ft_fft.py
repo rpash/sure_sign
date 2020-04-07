03c1577ad952bc184d71b91f129a1fb736e34f6e
Ray Aurelle P Puyat <rpuyat@umich.edu>
2020-04-07 00:53:47 -0400

    Converted data to grayscale and took fft of each img in data

--- src/features/ft_fft.py
+++ src/features/ft_fft.py
@@ -3,9 +3,13 @@ Featurization method for FFT
 """
 
 import numpy as np
+import cv2
 
 def featurize(data):
     # TODO implement this
     ret = []
-    ret = [np.fft.fft(data[i]) for i in data]
+    grayData = []
+    for i in data:
+        grayData.append(cv2.cvtColor(i, cv2.COLOR_BGR2GRAY))
+    ret = [np.fft.fft(i) for i in grayData]
     return ret
