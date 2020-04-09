849cdf0f09aed4f77c884fdbca7bce78c44a0400
Ray Aurelle P Puyat <rpuyat@umich.edu>
2020-04-09 02:55:23 -0400

    Started implementation on wavelet feature (unfinished)

--- src/features/ft_wavelet.py
+++ src/features/ft_wavelet.py
@@ -4,21 +4,17 @@ Featurization method for Wavelet
 
 import numpy as np
 import pywt
+import cv2
 
-def featurize(data):
-    # TODO implement this
-    
-    grayData = []
-    coeffs = []
+def featurize(data,txName):
     ret = []
-    A - []
-    grayData = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in data]
-    coeffs = [pywt.dwt2(grayData, 'haar') for img in grayData]
-    for item in coeffs:
-        cA, (cV, cH, cD) = item
-        A.append(cA)
-
+    grayData = []
+    fftData = []
     
-    ret = [coef.flatten() for coef in A]
+    grayData = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in data]
+    wlData = [pywt.dwt(img,txName) for img in grayData]
+    #TODO: wlData returns two arrays cA and cD. Don't know which one to return. Also cA and cD are not same shape as original grayscaled image
+    
     
     return ret
+
