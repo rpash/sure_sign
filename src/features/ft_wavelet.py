3d2d90050ef3c88f2ec01e3767234fbde22b80e6
hassounh <hassounh@umich.edu>
2020-04-11 13:58:34 -0400

    Update ft_wavelet.py

--- src/features/ft_wavelet.py
+++ src/features/ft_wavelet.py
@@ -7,4 +7,18 @@ import pywt
 
 def featurize(data):
     # TODO implement this
-    return pywt.dwt2(data, 'wavelet.name')
+    
+    grayData = []
+    coeffs = []
+    ret = []
+    A - []
+    grayData = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in data]
+    coeffs = [pywt.dwt2(grayData, 'haar') for img in grayData]
+    for item in coeffs:
+        cA, (cV, cH, cD) = item
+        A.append(cA)
+
+    
+    ret = [coef.flatten() for coef in A]
+    
+    return ret
