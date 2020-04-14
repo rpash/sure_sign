7282ffad1b6606b3b344676a8342fedfc238468b
Ray Aurelle P Puyat <rpuyat@umich.edu>
2020-04-14 18:18:58 -0400

    Added Gaussian High Pass Filter

--- src/features/ft_fft.py
+++ src/features/ft_fft.py
@@ -2,8 +2,21 @@
 Featurization method for FFT
 """
 
-import numpy as np
 import cv2
+import numpy as np
+from math import exp
+from scipy.spatial import distance
+
+#Gaussian High Pass Filter Code from: https://medium.com/@hicraigchen/digital-image-processing-using-fourier-transform-in-python-bcb49424fd82
+def gaussianHP(D0,imgShape):
+    base = np.zeros(imgShape[:2])
+    rows, cols = imgShape[:2]
+    center = (rows/2,cols/2)
+    for x in range(cols):
+        for y in range(rows):
+            base[y,x] = 1 - exp(((-distance.euclidean((y,x),center)**2)/(2*(D0**2))))
+    return base
+
 
 def featurize(data):
     """
@@ -15,9 +28,12 @@ def featurize(data):
         features: (N, M) matrix of N features of length M
     """
     if data.ndim > 3:
-        grey_data = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in data]
+        gray_data = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in data]
     else:
-        grey_data = data
-
-    features = np.array([np.fft.fft2(img).flatten() for img in grey_data])
+        gray_data = data
+    fft_data = [np.fft.fft2(img) for img in gray_data]
+    centered_data = [np.fft.fftshift(img) for img in fft_data]
+    centered_filtered_data = [img * gaussianHP(50,img.shape) for img in centered_data]
+    filtered_data = [np.fft.ifftshift(img) for img in centered_filtered_data] 
+    features = np.array([img.flatten() for img in filtered_data])
     return features
